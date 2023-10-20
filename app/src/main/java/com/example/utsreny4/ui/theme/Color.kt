630f97efc8309e14c58d1b71ac88130b2f1ae3cb
haqi111 <asidiqibaihaqi@gmail.com>
2023-10-20 20:19:21 +0700

    Program UTS Mata Kuliah PBM

---
+++ app/src/main/java/com/example/utsreny4/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.utsreny4.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD7CEEC)
+val PurpleGrey80 = Color(0xFF726A80)
+val Pink80 = Color(0xFF885E6A)
+
+val Purple40 = Color(0xFFCA7D97)
+val PurpleGrey40 = Color(0xFFE91E63)
+val Pink40 = Color(0xFFECA1B9)
